7f8b8dbed140870a12ce6faeda5c21ac2fcf457f
Elias Fernandes <elias.neto@rodobank.com.br>
2024-10-31 10:23:38 -0300

    ajuste visual e preparacao dos cruds

--- app/Views/spaces/home.php
+++ app/Views/spaces/home.php
@@ -7,9 +7,12 @@
     <title>Home</title>
 </head>
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+
     * {
         margin: 0;
         padding: 0;
+        font-family: "Open Sans", sans-serif;
     }
 
 
@@ -23,13 +26,58 @@
         align-items: center;
         justify-content: center;
         height: 100vh;
+        flex-direction: column;
+    }
+
+    .buttons {
+        width: 7vw;
+        height: 4vh;
+        background-color: #9a9da0;
+        border-radius: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+
+    }
+
+    .buttons a {
+        text-decoration: none;
+        color: #333333;
+    }
+
+    .nav {
+        display: flex;
+        gap: 3vw;
+        width: 50vw;
+        display: flex;
+        justify-content: space-around;
     }
 </style>
 
 <body>
 
     <section class="container">
-        <img src="https://img.freepik.com/free-vector/app-data-concept-illustration_114360-7834.jpg" alt="">
+        <div class="nav">
+            <div class="buttons">
+                <a href="/list">Spaces</a>
+            </div>
+            <div class="buttons">
+                <a href="/list">Customers</a>
+            </div>
+            <div class="buttons">
+                <a href="/list">Rentals</a>
+            </div>
+            <div class="buttons">
+                <a href="/list">Relation</a>
+            </div>
+
+        </div>
+
+        <div>
+            <img src="https://img.freepik.com/free-vector/app-data-concept-illustration_114360-7834.jpg" alt="">
+        </div>
+
     </section>
 
 
